1c3242da3504351d1093ab862dd08ff7be976fb2
seoil0404 <seoil6795@gmail.com>
2025-01-02 19:53:49 +0900

    Map Generate Ended

--- Assets/File_Seoil/Map/Stage.cs
+++ Assets/File_Seoil/Map/Stage.cs
@@ -1,17 +1,28 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Stage
 {
     public List<Stage> connect;
+    public Image objectSpriteRenderer;
 
     private GameObject allocatedStageObject;
     private RectTransform stageObjectRectTransform;
 
-    public Stage()
+    public StageType Type { get; private set; }
+
+
+    public enum StageType
+    {
+        SpecialCombat, Combat, Chest, Rest, Event, Boss
+    }
+
+    public Stage(StageType type)
     {
         connect = new List<Stage>();
+        Type = type;
     }
 
     public RectTransform rectTransform
@@ -35,7 +46,9 @@ public class Stage
         set
         {
             allocatedStageObject = value;
+
             stageObjectRectTransform = allocatedStageObject.GetComponent<RectTransform>();
+            objectSpriteRenderer = allocatedStageObject.GetComponent<Image>();
         }
     }
 }
