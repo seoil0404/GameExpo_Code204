2352d69b5c7ce0fc4fcd0ba649e7eb2e0d3756df
seoil0404 <seoil6795@gmail.com>
2025-02-19 00:49:08 +0900

    Update SceneController.cs

--- Assets/File_Seoil/SceneController.cs
+++ Assets/File_Seoil/SceneController.cs
@@ -17,44 +17,6 @@ public class SceneController : MonoBehaviour
     public void LoadScene(Scene.SceneType sceneType)
     {
         LoadScene(Scene.GetSceneNameByType(sceneType));
-        
-        switch (sceneType)
-        {
-            case Scene.SceneType.MapScene:
-                Scene.mapManager.IsAllowOpen = true;
-                Scene.mapManager.IsStatic = true; 
-                break;
-            case Scene.SceneType.ChestRoom:
-                Scene.mapManager.DisableMap();
-                Scene.mapManager.IsAllowOpen = true;
-                Scene.mapManager.IsStatic = false;
-                break;
-            case Scene.SceneType.GameScene:
-                Scene.mapManager.DisableMap();
-                Scene.mapManager.IsAllowOpen = true;
-                Scene.mapManager.IsStatic = false;
-                break;
-            case Scene.SceneType.EventRoom:
-                Scene.mapManager.DisableMap();
-                Scene.mapManager.IsAllowOpen = true;
-                Scene.mapManager.IsStatic = false;
-                break;
-            case Scene.SceneType.RestRoom:
-                Scene.mapManager.DisableMap();
-                Scene.mapManager.IsAllowOpen = true;
-                Scene.mapManager.IsStatic = false;
-                break;
-            case Scene.SceneType.ShopRoom:
-                Scene.mapManager.DisableMap();
-                Scene.mapManager.IsAllowOpen = true;
-                Scene.mapManager.IsStatic = false;
-                break;
-            default:
-                Scene.mapManager.DisableMap();
-                Scene.mapManager.IsAllowOpen = false;
-                Scene.mapManager.IsStatic = false;
-                break;
-        }
     }
 
     public void LoadScene(string sceneName)
