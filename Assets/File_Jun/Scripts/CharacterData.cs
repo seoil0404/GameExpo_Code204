31912c375e429afb2993ee3912ad61eb3c686f54
cranberryandmixnuts <156453883+cranberryandmixnuts@users.noreply.github.com>
2025-02-18 13:11:37 +0900

    준이코드 재수정

--- Assets/File_Jun/Scripts/CharacterData.cs
+++ Assets/File_Jun/Scripts/CharacterData.cs
@@ -19,4 +19,36 @@ public class CharacterData
         currentHp = maxHp;
         currentUltimateGauge = 0;
     }
+
+    public int GetMaxHp() //채현
+    {
+        return maxHp;
+    }
+
+    public void SetMaxHp(int value) //채현
+    {
+        maxHp = value;
+        if (currentHp > maxHp)
+        {
+            currentHp = maxHp;
+        }
+    }
+
+    public int GetCurrentHp() //채현
+    {
+        return currentHp;
+    }
+
+    public void SetCurrentHp(int value) //채현
+    {
+        currentHp = value;
+        if (currentHp > maxHp)
+        {
+            currentHp = maxHp;
+        }
+        else if (currentHp < 0)
+        {
+            currentHp = 0;
+        }
+    }
 }
