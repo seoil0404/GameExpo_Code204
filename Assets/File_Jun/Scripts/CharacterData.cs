0fee939f4fa9b93bcd4984540736c321f38d2f07
cranberryandmixnuts <156453883+cranberryandmixnuts@users.noreply.github.com>
2025-02-18 12:42:34 +0900

    준이코드 수정

--- Assets/File_Jun/Scripts/CharacterData.cs
+++ Assets/File_Jun/Scripts/CharacterData.cs
@@ -1,22 +1,68 @@
 using UnityEngine;
 
 [System.Serializable]
-public class CharacterData
+public class CharacterData : MonoBehaviour
 {
+    public static CharacterData Instance { get; private set; } //채현
+
     public string characterName;
-    public int maxHp;
     public int ultimateGaugeMax;
     public string ultimateSkillDescription;
     public GameObject characterPrefab;
 
-    [HideInInspector]
+    public int maxHp;
     public int currentHp;
-    [HideInInspector]
     public int currentUltimateGauge;
 
-    public void Initialize()
+    private void Awake() //채현
     {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Initialize(int initialMaxHp)
+    {
+        maxHp = initialMaxHp;
         currentHp = maxHp;
         currentUltimateGauge = 0;
+    }
+
+    public int GetMaxHp() //채현
+    {
+        return maxHp;
+    }
+
+    public void SetMaxHp(int value) //채현
+    {
+        maxHp = value;
+        if (currentHp > maxHp)
+        {
+            currentHp = maxHp;
+        }
+    }
+
+    public int GetCurrentHp() //채현
+    {
+        return currentHp;
+    }
+
+    public void SetCurrentHp(int value) //채현
+    {
+        currentHp = value;
+        if (currentHp > maxHp)
+        {
+            currentHp = maxHp;
+        }
+        else if (currentHp < 0)
+        {
+            currentHp = 0;
+        }
     }
 }
