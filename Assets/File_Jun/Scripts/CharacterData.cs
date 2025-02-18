9531e693fdb9d4972255e6079ae685c41cb4dd23
Potato1016 <esky1004@gmail.com>
2025-02-18 13:01:56 +0900

    Revert "준이코드 수정"
    This reverts commit 0fee939f4fa9b93bcd4984540736c321f38d2f07.

--- Assets/File_Jun/Scripts/CharacterData.cs
+++ Assets/File_Jun/Scripts/CharacterData.cs
@@ -1,68 +1,22 @@
 using UnityEngine;
 
 [System.Serializable]
-public class CharacterData : MonoBehaviour
+public class CharacterData
 {
-    public static CharacterData Instance { get; private set; } //채현
-
     public string characterName;
+    public int maxHp;
     public int ultimateGaugeMax;
     public string ultimateSkillDescription;
     public GameObject characterPrefab;
 
-    public int maxHp;
+    [HideInInspector]
     public int currentHp;
+    [HideInInspector]
     public int currentUltimateGauge;
 
-    private void Awake() //채현
+    public void Initialize()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    public void Initialize(int initialMaxHp)
-    {
-        maxHp = initialMaxHp;
         currentHp = maxHp;
         currentUltimateGauge = 0;
-    }
-
-    public int GetMaxHp() //채현
-    {
-        return maxHp;
-    }
-
-    public void SetMaxHp(int value) //채현
-    {
-        maxHp = value;
-        if (currentHp > maxHp)
-        {
-            currentHp = maxHp;
-        }
-    }
-
-    public int GetCurrentHp() //채현
-    {
-        return currentHp;
-    }
-
-    public void SetCurrentHp(int value) //채현
-    {
-        currentHp = value;
-        if (currentHp > maxHp)
-        {
-            currentHp = maxHp;
-        }
-        else if (currentHp < 0)
-        {
-            currentHp = 0;
-        }
     }
 }
