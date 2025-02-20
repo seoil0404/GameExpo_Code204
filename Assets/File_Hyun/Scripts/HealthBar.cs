1f908ff7271aadbeba94c0c13f0e86209aee7f8e
cranberryandmixnuts <156453883+cranberryandmixnuts@users.noreply.github.com>
2025-02-20 22:37:22 +0900

    hp 바 완성

--- Assets/File_Hyun/Scripts/HealthBar.cs
+++ Assets/File_Hyun/Scripts/HealthBar.cs
@@ -3,54 +3,42 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    //[SerializeField] private Character[] characters = null;
+    public static HealthBar Instance { get; private set; }
+
+    [SerializeField] private Character[] characters = null;
     public Text Health;
 
     public Image CurrentHpBar;
     public Image DamageInflicted;
     public Image heeledHp;
 
-    private int MaxHp = 100; //characters[GameData.SelectedCharacterIndex - 1].characterData.MaxHp
-    private int CurrentHp = 60; //characters[GameData.SelectedCharacterIndex - 1].characterData.CurrentHp
     private int FirstHp;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void Start()
     {
         heeledHp.fillAmount = 0;
-        FirstHp = CurrentHp;
-        UpdateHpBar();
-    }
-
-    public void IncreaseMaxHp()
-    {
-        MaxHp++;
-        UpdateHpBar();
-    }
-
-    public void DecreaseMaxHp()
-    {
-        MaxHp--;
-        UpdateHpBar();
-    }
-
-    public void IncreaseCurrentHp()
-    {
-        CurrentHp++;
-        UpdateHpBar();
-    }
-
-    public void DecreaseCurrentHp()
-    {
-        CurrentHp--;
+        FirstHp = characters[GameData.SelectedCharacterIndex - 1].characterData.CurrentHp;
         UpdateHpBar();
     }
 
     void UpdateHpBar()
     {
-        Health.text = CurrentHp + " / " + MaxHp;
-        float current = CurrentHp, max = MaxHp, first = FirstHp;
+        Health.text = characters[GameData.SelectedCharacterIndex - 1].characterData.CurrentHp + " / " + characters[GameData.SelectedCharacterIndex - 1].characterData.MaxHp;
+        float current = characters[GameData.SelectedCharacterIndex - 1].characterData.CurrentHp, max = characters[GameData.SelectedCharacterIndex - 1].characterData.MaxHp, first = FirstHp;
         DamageInflicted.fillAmount = first / max;
-        if (CurrentHp > FirstHp)
+        if (characters[GameData.SelectedCharacterIndex - 1].characterData.CurrentHp > FirstHp)
         {
             CurrentHpBar.fillAmount = first / max;
             heeledHp.fillAmount = current / max;
