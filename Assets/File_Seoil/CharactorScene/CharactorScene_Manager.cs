fc075ac687dbcedb27ec06f7f1147b1fada8da2c
seoil0404 <seoil6795@gmail.com>
2025-02-23 11:07:53 +0900

    CharacterScene Added

--- Assets/File_Seoil/CharactorScene/CharactorScene_Manager.cs
+++ Assets/File_Seoil/CharactorScene/CharactorScene_Manager.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -8,11 +9,18 @@ public class CharactorScene_Manager : MonoBehaviour
 
     [Header("MonoBehaviour")]
     [SerializeField] private Image characterImage;
+    [SerializeField] private Canvas characterTextCanvas;
+
+    [Header("Prefabs")]
+    [SerializeField] private GameObject characterTextPrefab;
 
     [Header("Sprite")]
     [SerializeField] private Sprite Isa;
     [SerializeField] private Sprite Blayrin;
     [SerializeField] private Sprite Hugh;
+    [SerializeField] private Sprite IsaTextImage;
+    [SerializeField] private Sprite BlayrinTextImage;
+    [SerializeField] private Sprite HughTextImage;
 
     [Header("Sound")]
     public AudioSource audioSource;
@@ -21,10 +29,16 @@ public class CharactorScene_Manager : MonoBehaviour
     [SerializeField] private AudioClip HughSound;
     [SerializeField] private AudioClip EscSound;
 
+    [Header("Animation Setting")]
+    [SerializeField] private float characterTextStartInterval;
+    [SerializeField] private float characterTextAnimationDuration;
+
+    private GameObject currentCharacterText;
+    private Image currentCharacterTextImage;
+
     private void Awake()
     {
-        GameData.SelectedCharacterIndex = 1;
-        characterImage.sprite = Isa;
+        SetCharacter(1);
     }
 
     private void Update()
@@ -39,21 +53,41 @@ public class CharactorScene_Manager : MonoBehaviour
     public void SetCharacter(int characterIndex)
     {
         GameData.SelectedCharacterIndex = characterIndex;
+
+        if(currentCharacterText != null) Destroy(currentCharacterText);
+        currentCharacterText = Instantiate(characterTextPrefab, characterTextCanvas.transform);
+
+        currentCharacterTextImage = currentCharacterText.GetComponent<Image>();
+
+        Vector2 originalPos = currentCharacterText.GetComponent<RectTransform>().anchoredPosition;
+
+        currentCharacterText.GetComponent<RectTransform>().position = new Vector2(
+            currentCharacterText.GetComponent<RectTransform>().anchoredPosition.x - characterTextStartInterval, 
+            currentCharacterText.GetComponent<RectTransform>().anchoredPosition.y
+            );
+
+        currentCharacterText.GetComponent<RectTransform>().DOMoveX(originalPos.x, characterTextAnimationDuration);
+
         switch (characterIndex)
         {
             case 1:
                 characterImage.sprite = Isa;
+                currentCharacterTextImage.sprite = IsaTextImage;
                 PlaySound(IsaSound);
                 break;
             case 2:
                 characterImage.sprite = Blayrin;
+                currentCharacterTextImage.sprite = BlayrinTextImage;
                 PlaySound(BlayrinSound);
                 break;
             case 3:
                 characterImage.sprite = Hugh;
+                currentCharacterTextImage.sprite = HughTextImage;
                 PlaySound(HughSound);
                 break;
         }
+
+        characterImage.transform.DOShakePosition(0.1f, 7);
     }
 
     void PlaySound(AudioClip clip)
