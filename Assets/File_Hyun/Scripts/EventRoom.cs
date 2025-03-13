c4e9d48a1f4fad0aba2a395ac86c890a1a3b09af
cranberryandmixnuts <156453883+cranberryandmixnuts@users.noreply.github.com>
2025-03-13 20:05:36 +0900

    .

--- Assets/File_Hyun/Scripts/EventRoom.cs
+++ Assets/File_Hyun/Scripts/EventRoom.cs
@@ -1,11 +1,16 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using static EnemyData;
 
 public class EventRoom : MonoBehaviour
 {
     [SerializeField] private Character[] characters = null;
+
+    public CombatData combatData;
+    public GameObject level_1;
+    public GameObject level_2;
+    public GameObject level_3;
 
     public GoldData goldData;
     public GameObject eventUI;
@@ -28,6 +33,26 @@ public class EventRoom : MonoBehaviour
         ChoiceButtons.SetActive(true);
         EscObject.SetActive(false);
         TriggerRandomEvent();
+
+        level_1.SetActive(false);
+        level_2.SetActive(false);
+        level_3.SetActive(false);
+
+        switch (combatData.HabitatType)
+        {
+            case HabitatType.Forest:
+                level_1.SetActive(true);
+                break;
+            case HabitatType.Castle:
+                level_2.SetActive(true);
+                break;
+            case HabitatType.DevilCastle:
+                level_3.SetActive(true);
+                break;
+            default:
+                Debug.LogWarning("Unknown Habitat Type!");
+                break;
+        }
     }
 
     private void TriggerRandomEvent()
