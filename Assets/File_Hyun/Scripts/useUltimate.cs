76db682f0f5870f2729202a15e3e9ce0b02356ad
cranberryandmixnuts <156453883+cranberryandmixnuts@users.noreply.github.com>
2025-03-27 23:40:45 +0900

    버튼 쿨타임 추가

---
+++ Assets/File_Hyun/Scripts/useUltimate.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class useUltimate : MonoBehaviour
+{
+    public void UseUltimate()
+    {
+        // 선택된 캐릭터의 데이터를 가져옵니다.
+        var characterData = CharacterManager.selectedCharacter.characterData;
+
+        // 궁극기 게이지가 꽉 찼는지 확인합니다.
+        if (characterData.CurrentUltimateGauge >= characterData.MaxUltimateGauge)
+        {
+            if (characterData.ultimateSkill != null)
+            {
+                // 궁극기 스킬 활성화: Grid.instance을 인자로 전달합니다.
+                characterData.ultimateSkill.ActivateUltimate(CharacterManager.selectedCharacter, Grid.instance);
+                // 궁극기 사용 후 게이지 초기화 및 저장
+                characterData.CurrentUltimateGauge = 0;
+                CharacterManager.SaveUltimateGauge();
+                Debug.Log("궁극기 발동!");
+            }
+            else
+            {
+                Debug.LogWarning("궁극기 스킬이 할당되어 있지 않습니다.");
+            }
+        }
+        else
+        {
+            Debug.Log("궁극기 게이지가 다 차지 않았습니다.");
+        }
+    }
+}
